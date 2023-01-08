8b63751c8e52691b0bff1b9894d26a77211899e3
minkyu <tour00389@gmail.com>
2023-01-08 14:26:45 +0900

    [Comment] Add comment in deliveryService

--- back/src/services/deliveryService.js
+++ back/src/services/deliveryService.js
@@ -1,12 +1,14 @@
 import { Delivery } from "../db/model/Delivery";
 
 class deliveryService {
+  // 해당 거래 배송 조회
   static async deliveryDetail(dealId) {
     const delivery = await Delivery.findDeliveryByDealId(dealId);
 
     return delivery;
   }
 
+  //배송상태별 배송 조회
   static async deliveryByStatus(status) {
     const delivery = await Delivery.findDeliveryByStatus(status);
 
